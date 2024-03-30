bae83b166f3c94cb1052a7d2bc80e582c02763f3
Shadman Jamil <shadman.jamil@gmail.com>
2024-03-30 04:20:59 +0100

    updated with comments

--- service-layer/src/main/java/com/shan/servicelayer/service/RoomReservationService.java
+++ service-layer/src/main/java/com/shan/servicelayer/service/RoomReservationService.java
@@ -29,6 +29,7 @@ public class RoomReservationService {
     }
 
     public List<RoomReservation> getRoomReservationForDate(String reservationDate){
+
         Date date = null;
         if (StringUtils.isNotEmpty(reservationDate)) {
             date = Date.valueOf(reservationDate);
@@ -37,27 +38,35 @@ public class RoomReservationService {
         }
 
         Map<Long, RoomReservation> roomReservations = new HashMap<>();
+
+        // Getting Rooms and setting all rooms reservations in array
         List<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(room -> {
             RoomReservation roomReservation = new RoomReservation();
             roomReservation.setRoomId(room.getId());
             roomReservation.setRoomName(room.getName());
             roomReservation.setRoomNumber(room.getRoomNumber());
+
             roomReservations.put(roomReservation.getRoomId(), roomReservation);
         });
 
+        // Getting existing reservation of desired date from database
         List<Reservation> reservations = this.reservationRepository.findAllByReservationDate(date);
         reservations.forEach(reservation -> {
+
+            // Updating all room reservations with id and date after fetching from DB
             RoomReservation roomReservation = roomReservations.get(reservation.getRoomId());
             roomReservation.setReservationId(reservation.getId());
             roomReservation.setReservationDate(reservation.getReservationDate().toString());
 
+            // Fetching guest information of reservation to update room reservations
             Optional<Guest> guest = this.guestRepository.findById(reservation.getGuestId());
             roomReservation.setGuestId(guest.get().getId());
             roomReservation.setFirstName(guest.get().getFirstName());
             roomReservation.setLastName(guest.get().getLastName());
         });
 
+        // converting into list to return
         return roomReservations.values().stream().toList();
     }
 
